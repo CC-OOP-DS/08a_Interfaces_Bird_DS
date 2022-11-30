d58ef39123325ac37ee463f3840e568d70a62b36
Daniel <schindlerdaniel@web.de>
2022-11-30 10:33:46 +0100

    interface1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,9 @@ public class App {
     
     public static void main(String[] args) {
      
+        Bird bird = new Bird();
+        output(bird.fly());
+
     }
 
     public static void output(String outputStr) {
